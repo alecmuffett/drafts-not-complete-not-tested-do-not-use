6a85a766c9b89a5ae42bd0471abf0708c993b065
Alec Muffett <alec.muffett@gmail.com>
2016-12-10 17:01:43 +0000

    tweaks and new tor repo

--- onion-cluster-ideas/download-and-build-tor.sh
+++ onion-cluster-ideas/download-and-build-tor.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
 
-url=https://www.torproject.org/dist/tor-0.2.8.10.tar.gz
+url=https://www.torproject.org/dist/tor-0.2.8.11.tar.gz
 tarball=`basename $url`
 dir=`basename $url .tar.gz`
 
-sudo aptitude install libevent-dev zlib1g-dev libssl-dev || exit 1
+packages="
+haveged
+ifstat
+libevent-dev
+libssl-dev
+zlib1g-dev
+"
+
+sudo aptitude install $packages || exit 1
+
 wget $url || exit 1
 tar xvfz $tarball || exit 1
 cd $dir || exit 1
 ./configure || exit 1
 make || exit 1
-sudo make install || exit 1
+
+## sudo make install || exit 1
+
 exit 0
--- onion-cluster-ideas/polytunnel.sh
+++ onion-cluster-ideas/polytunnel.sh
@@ -1,22 +1,41 @@
 #!/bin/sh
+
 root=`dirname $0`
 cd $root
 root=`pwd`
-tor=./tor-0.2.8.10/src/or/tor
-for i in 0 1 2 3 4 5 6 7 8 9 ; do # works for up-to ..9
+
+tor=./tor-0.2.8.11/src/or/tor
+
+# for testing I will run 3x daemons on 7 nodes = 21 daemons
+# compute: ceiling(60 / 21) = 3 = three intropoints/daemon
+num_daemons_per_node=3
+num_intro_points=3
+
+# ports are based on this number (starting at N+1)
+portroot=8500
+
+i=1
+while [ $i -le $num_daemons_per_node ] ; do
     hs=hs$i
     dir=$root/$hs.d
-    port=900$i
+    port=`expr $portroot + $i`
+
     test -d $dir || mkdir $dir || exit 1
     chmod 700 $dir
+
     if [ ! -f $dir/config ] ; then
         (
             echo DataDirectory $dir
             echo HiddenServiceDir $dir/
             echo HiddenServicePort $port localhost:$port
+	    echo HiddenServiceNumIntroductionPoints $num_intro_points
             echo SOCKSPort 905$i
         ) > $dir/config
     fi
+
     $tor --hush -f $dir/config &
+
+    i=`expr $i + 1`
 done
+
 wait
